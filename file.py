271ce8fa3d433ac427e2fb6505e3e6499f36d1d0
jeamin9849 <jeamin9849@gmail.com>
2026-05-12 15:44:26 +0900

    0512 15:43

---
+++ file.py
@@ -0,0 +1,10 @@
+import csv
+from scrapper import search_incruit
+
+# jobs = search_incruit("파이썬", 1)
+def save_to_csv(jobs):
+    with open("downloads.csv", "w", encoding = "cp949")as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(["No", "회사", "제목", "지역", "상세보기 페이지"]) #writerow : 한 줄씩 집어넣음
+        for i, job in enumerate(jobs):
+            csv_writer.writerow([i+1, job["company"], job["title"], job["location"], job["link"]]) 
--- scrapper.py
+++ scrapper.py
@@ -1,25 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 
-keyword="파이썬"
-url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=0"
+def search_incruit(keyword,pages):
+
+
+    jobs = []
+    for i in range(pages):
+        page = i * 30
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}"
+
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, "html.parser") #BeautifulSoup라는 class를 통해 soup라는 객체를 만듬
+
+        lis = soup.find_all("li", class_="c_col")   # 웹내 특정 디자인을 가져오는 명령, css중 c_col이라는 명이 담긴걸 모두 가져옴
+        # print(len(lis))
+        # print(lis[0])
+
+
+
+        for li in lis:
+            company = li.find("a", class_="cpname").text.strip() # text.strip :  원하는 텍스트만 출력+공백 제거
+            # print(company)
+            title = li.find("div", class_="cell_mid").find("div", class_= "cl_top").find("a").text #cell_mid테그 안에 cl_top 안에 a 테그 안에 텍스트
+            # print(title)
+            location = li.find("div", class_="cl_md").find_all("span")[0].text.strip()
+            # print(location)
+            link = li.find("div", class_= "cell_mid").find("div", class_ = "cl_top").find("a").get("href") #...에서 get함수로 href 의 내용을 가져옴
+            # print(link)
+            
+            job_data = {
+                "company" : company,
+                "title" : title,
+                "location" : location,
+                "link" : link        
+            }
+
+            jobs.append(job_data)
+
+            
+    return jobs
+
+# def search_saramin(keyword,pages):
+#     url = f"https://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=search&searchword=%ED%8C%8C%EC%9D%B4%EC%8D%AC&recruitPage={page}&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd={keyword}&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n"
+
+
 response = requests.get(url)
-# print(response.text)
+print(response.status_code) # status_code는 보통 HTTP 요청 결과 상태를 나타내는 값 200 : 성공, 404 : 페이지 없음
+print(response.text)
 
-soup = BeautifulSoup(response.text, "html.parser")
-
-lis = soup.find_all("li", class_="c_col")   # 웹내 특정 디자인을 가져오는 명령, css중 c_col이라는 명이 담긴걸 모두 가져옴
-# print(len(lis))
-# print(lis[0])
-
-
-
-for li in lis[0:1]:
-    # company = li.find("a", class_="cpname").text.strip() # text.strip :  원하는 텍스트만 출력+공백 제거
-    # print(company)
-    # title = li.find("div", class_="cell_mid").find("div", class_= "cl_top").find("a").text #cell_mid테그 안에 cl_top 안에 a 테그 안에 텍스트
-    # print(title)
-    # location = li.find("div", class_="cl_md").find_all("span")[0].text.strip()
-    # print(location)
-    link = li.find("div", class_= "cell_mid").find("div", class_ = "cl_top").find("a").get("href")
-    print(link)
