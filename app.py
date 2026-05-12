4a8f719fcb114b6993811e7c10e661dcc9968d00
jeamin9849 <jeamin9849@gmail.com>
2026-05-12 16:27:14 +0900

    requirements

--- app.py
+++ app.py
@@ -31,7 +31,4 @@ def file():
 #     return render_template("hello.html")
 
 if __name__ == '__main__':
-    app.run(
-            host='0.0.0.0',
-            port=int(os.environ.get("PORT", 5000))
-        )
+    app.run(debug=True)
